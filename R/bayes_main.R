d06c7fff3bde8370355d1c6b3fe3c8eba8c5a0e1
Mark K <mtkurzej@umich.edu>
2016-04-29 01:58:15 -0400

    Fixed Chebyshev Error on precision
    There was an error in the program that made the program calculate the
    incorrect value of n for what we wanted to accomplish. So this bug was
    fixed

--- R/bayes_main.R
+++ R/bayes_main.R
@@ -15,7 +15,7 @@
 #' }
 #' chebyshev_compute(f, lower = -2, upper = 2, tol = 1E-10)
 
-chebyshev_compute <- function(fxn, lower, upper, tol = 1E-15, n_subdivisions = 100, maxN = 3000, maxEfficiency = F) {
+chebyshev_compute <- function(fxn, lower, upper, tol = 1E-10, n_subdivisions = 100, maxN = 3000, maxEfficiency = F) {
   # Start with three points - a quadratic approximation
   N = 2
   
@@ -26,21 +26,22 @@ chebyshev_compute <- function(fxn, lower, upper, tol = 1E-15, n_subdivisions = 1
     f_points = -cos(k * pi / N)
     f_points_scaled = (f_points + 1) / 2 * (upper - lower) + lower
     # Calculate the maximum difference
-    diff = laply(.data = seq(from = lower + 1E-14, to = upper - 1E-14, length = n_subdivisions), .fun = function(x){
+    diff = sapply(seq(from = lower, to = upper, length = n_subdivisions), function(x) {
       # Move the point from [lower,upper]->[-1,1]
       x <- 2 * (x - lower) / (upper - lower) - 1
       # Ensure that y_j points in [lower,upper] are usable
       p_x = sum(weights * (-1)^k * fxn(f_points_scaled) / (x - f_points)) / sum(weights * (-1)^k / (x - f_points))
        # Return the absolute deviation
-      browser()
-      abs(p_x - fxn(x))
+      abs(p_x - fxn((x + 1) / 2 * (upper - lower) + lower))
     })
     # Break if tolerance is good, else evaluate updating conditions
-    cat(sprintf("Max Diff in Digits: %f\n", abs(log10(max(diff, na.rm = T)))))
+    cat(sprintf("Max Diff Digits: %*.5f | ", 8, abs(log10(max(diff, na.rm = T)))))
+    cat(sprintf("ActualTol: %.5E\n", max(diff,na.rm=T)))
     if (max(diff, na.rm = T) < tol) {
       break
     } else if (N > maxN) {
-      stop("The function needs a lot of precision, function evaluation will stop past 3000 maxN")
+      cat("The function needs a lot of precision, function evaluation stops past N > maxN")
+      break
     } else if (maxEfficiency) {
       # in maxEfficiency, we attempt to find the optimum number of points
       N = N + 1 
@@ -51,21 +52,24 @@ chebyshev_compute <- function(fxn, lower, upper, tol = 1E-15, n_subdivisions = 1
   }
   cat("Choice of n:", N, "\n")
   # Now we return a function that interpolates between [lower,upper] for our function
+  # N = 50
   function(val) {
+    # Ensure we are not extrapolating
+    stopifnot(all(val >= lower) & all(val <= upper))
+    
     # Calculate the weights and the x_j
     weights = c(1/2, rep(1, N-1), 1/2)
     k = 0:N
     f_points = -cos(k * pi / N)
-    
-    # Ensure we are not extrapolating
-    stopifnot(val >= lower & val <= upper)
-    
+    # Ensure that y_j points in [lower,upper] are usable
+    f_points_scaled = (f_points + 1) / 2 * (upper - lower) + lower
+  
     # For each point to eval, compute the polynomial approximation
     result = sapply(val, function(x) {
+      
       # Scale x to the [-1,1] interval
       x <- 2 * (x - lower) / (upper - lower) - 1
-      # Ensure that y_j points in [lower,upper] are usable
-      f_points_scaled = (f_points + 1) / 2 * (upper - lower) + lower
+
       p_x = sum(weights * (-1)^k * fxn(f_points_scaled) / (x - f_points)) / sum(weights * (-1)^k / (x - f_points))
     })
     return(result)
@@ -75,17 +79,18 @@ chebyshev_compute <- function(fxn, lower, upper, tol = 1E-15, n_subdivisions = 1
 }
 
 f <- function(x) {
-  cos(x)
+  exp(x) * sin(x) * gamma(x) * exp(abs(x))
 }
 
-l = -2
-u = 2
+l = 1
+u = 3
 # laply(seq(1/2,8), function(x) {
-  myfunc = chebyshev_compute(f, lower = l, upper = u, tol = 10^(-8), maxEfficiency = T, n_subdivisions = 1000)
-  curve(f, l,u, lty = 2, col = "red")
-  curve(myfunc, l,u, add = T, col = "blue", lty = 4, pch = 2)
+  myfunc = chebyshev_compute(f, lower = l, upper = u, maxEfficiency = T, n_subdivisions = 1000)
+  curve(f, l,u, lty = 2, col = "red", n = 300)
+  curve(myfunc, l,u, add = T, col = "blue", lty = 4, pch = 2, n = 300)
   p = seq(l,u, length = 1000)
-  plot(log10(abs(f(p) - myfunc(p))), type = "p", ylim = c(-15,0))
+  plot(log10(abs(f(p) - myfunc(p))), type = "s", ylim = c(-15,0))
+  abline(h = log10(1E-10))
 # })
 
 
