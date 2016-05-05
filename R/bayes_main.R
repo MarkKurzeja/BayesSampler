187717dcb69d7ef55793550ab04e6ee20a3d862d
Mark K <mtkurzej@umich.edu>
2016-05-04 22:40:44 -0400

    Added chebfun, parabolaInterpolate, meroSFunc, and CInfParabolaSmoother
    List of updates:
    + chebfun: Return a function that allows one to replace fxn with
    chebfun(fxn) and easily evaluate a function to machine precision with a
    Chebfun. This function uses the coefficients of a chebyshev
    approximation as opposed to the interpolating polynomial and it is very
    good at projecting the fxn onto a chebyshev series
    + parabolaInterpolate: return the polynomial interpolate through three
    points - this function is hard-coded so it is really fast
    + meroSFunc: A function that takes in x \in [0,1] and returns a weight
    from a meromorphic CInfinity function that is infinity differentiable.
    It is useful for constructing spline functions using only parabolas
    + CInfParabolaSmoother: This function, with inspiration from Ulrich
    Multze, uses a meromorphic CInfinity weight function given by: 1 / (1 +
    exp(p * (1/ x + 1 / (x -1)))) with p = 0.810747. This function allows
    you to interpolate a set of (x,y) pairs perfectly with minimal work and
    quick functional execution
    + Added a new return value for chebyshev_bestN: Instead of returning
    just a value of N that satisfies the criteria for the error, this
    function now returns the coefficients as well. They are all stored in a
    list
    
    - Misc. Code at the bottom

--- R/bayes_main.R
+++ R/bayes_main.R
@@ -134,7 +134,7 @@ chebyshev_coef <- function(fxn, lower, upper, tol = 1E-10, maxN = 10000, verbose
 #' This function takes in a minimum and a maximum range, a function, and a 
 #' tolerance, and returns a the chebyshev coefficients of a function - useful 
 #' for determining the proper N to use. Sources: SIAM Chebyshev Expansions
-#' EQ3.62
+#' EQ3.62. It returns the best value of N and the coefficients that produce it
 #' @keywords chebyshev approximation
 #' @param fxn The function we wish to approximate
 #' @param lower The lower bound of the approximation of the range
@@ -161,22 +161,193 @@ chebyshev_bestN <- function(fxn, lower, upper, tol = 1E-10, maxN = 10000, verbos
   # 3) Use the values from (2) to determine the index of the coefficient that
   #    is not
   bestN = max(which(!(1:length(coefs) %in% which(abs(coefs) < tol)))) + 1
-  return(bestN)
+  coefs = coefs[1:bestN]
+  return(list(bestN = bestN, coefs = coefs))
 }
 
+#' Approximate a function with a Chebyshev Series
+#' 
+#' This function takes in a minimum and a maximum range, a function, and a 
+#' tolerance, and returns a chebyshev series evalued based on the coefficients
+#' of the series. This function is actually faster than the interpolation
+#' function for a simular value of N
+#' Sources: SIAM Chebyshev Expansions: EQ3.62
+#' for coefficients Algorithm 3.2 for evaluating the sum (Modified Clenshaws
+#' algorithm) EQ3.70 for error bounds
+#' @keywords chebyshev approximation
+#' @param fxn The function we wish to approximate
+#' @param lower The lower bound of the approximation of the range
+#' @param upper The upper bound of the approximation of the range
+#' @param tol Any coefficient smaller than this will be disreguarded
+#' @param maxN The maximum number of coefficients to calculate before the 
+#'   program errors and says there is no resolution to this approximation
+#' @param verbose logical; if true, the chebyshev_coef function will print a 
+#'   verbose message output indicating the coefficient sizes and the amount of 
+#'   digits (base 10) that each coefficient represents
+#' @export
+#' @examples
+#' f <- function(x) {
+#'    x^5
+#' }
+#' chebfun(f, N = 1, lower = l, upper = u, tol = 1E-15)
 
-f <- function(x) {
-    sin(6*x) + sign(sin(x+exp(2*x)))
-   # x + sin(100 * x^3)
+
+chebfun <- function(fxn, lower, upper, tol = 1E-10, maxN = 10000, verbose = F) {
+  # Get the coefficients and truncate them to the largest N
+  coef_list = chebyshev_bestN(fxn = fxn, lower = lower, upper = upper, tol = tol, maxN = maxN)
+  N = coef_list$bestN - 1
+  coef = coef_list$coefs
+  
+  result <- function(x) {
+    # Ensure x is in [lower, upper]
+    if (any(x < lower) | any(x > upper)) {
+      stop("x is not in [lower,upper] - extrapolation is not stable")
+    }
+    
+    # Scale the x such that they are in [-1,1]
+    x <- (x - lower) / (upper - lower) * 2 - 1
+    
+    y_val <- sapply(x, function(loopx) {
+      weights = c(1/2, rep(1,N - 1), 1/2) 
+      # browser()
+      sum(weights * coef * cos(0:N * acos(loopx)))
+    })
+    return(y_val)
+  }
+    return(result)
+}
+  
+#' Meromorphic function for function weighting 
+#' 
+#' Meromorphic Sinosoid function that makes this possible Very cool properties - bounded, parameterized, monotonic, C^Inf ' Sources: Ulrich Multz Documentation on using this function for a parabolic interpolation program 
+#' @keywords parabola meromorphic weighting weight
+#' @param x The input value to the function x \in [0,1] '
+#' @export 
+#' @examples  
+#' meroSFunc(0:100 / 100)
+meroSFunc <- function(x) {
+  # Set a value of p to 0.810747 which minimizes the L2 norm of the second
+  # derivative
+  p = 0.810747
+
+  # Vectorize the function
+  sapply(x, function(loopx) {
+    if (loopx <= 0)
+    { 
+      return(0)
+    } else if(loopx >= 1){
+      return(1)
+    }
+    
+    return(1 / (1 + exp(p * (1 / loopx + 1 / (loopx - 1)))))
+  })
+};
+
+curve(meroSFunc,-1/2,3/2)
+
+
+#' Interpolate a function with a parabola 
+#' 
+#' Take in a vector of x values and a vector of y values for a parabola, three in total, and return the parabola approximation at the value of x 
+#' @keywords parabola interpolation interpolate
+#' @param x The input value to the function
+#' @param xvec A vector of length three of the x values
+#' @param yvec A vector of length three of the y values
+#' @export 
+#' @examples  
+#' parabolaInterpolate(0.5, c(1,0,4), c(5,6,2))
+parabolaInterpolate <- function(x, xvec, yvec) {
+  sapply(x, function(loopx) {
+    yvec[1] * (loopx - xvec[2])*(loopx - xvec[3]) / ((xvec[1] - xvec[2])*(xvec[1] - xvec[3])) +
+    yvec[2] * (loopx - xvec[1])*(loopx - xvec[3]) / ((xvec[2] - xvec[1])*(xvec[2] - xvec[3])) +
+    yvec[3] * (loopx - xvec[1])*(loopx - xvec[2]) / ((xvec[3] - xvec[1])*(xvec[3] - xvec[2]))
+  })
 }
 
-l = -3
-u = -1
-curve(f, l, u, n = 1000)
+#' C^Inf Parabola Smoother - Ulrich Multze
+#' 
+#' Take in a vector of x and y values and return the meromorphic weighting of parabola splines as suggested by Ulrich Multz. This function is resulting C^Inf meaning all derivatives are continuious!
+#' This funcion returns a function that allows you to compute this for an arbitrary x
+#' @keywords parabola interpolation interpolate
+#' @param x The input value to the function
+#' @param xvec A vector of the x values
+#' @param yvec A vector of the y values
+#' @export 
+#' @examples  
+#' This example aims to show the boundary polynomials that are used in this 
+#' method of interpolation. The black line is the meromorphic weight of the two
+#' interpolating polynomials that are to the left and to the right of the 
+#' given value of x
+#' xvec = 1:10
+#' yvec = runif(10) * x
+#' Get the interpolating function
+#' myfunc <- CInfParabolaSmoother(xvec,yvec)
+#' Plot the curve and the points
+#' curve(myfunc, 1,10)
+#' points(cbind(xvec,yvec), pch = 3)
+#' Draw in the boundary polynomials
+#' for (i in 2:9) {
+#' curve(parabolaInterpolate(x, xvec[seq(i-1,i+1)],yvec[seq(i-1,i+1)]), xvec[i-1], xvec[i + 1], add = T, col = "blue", lty = 2)
+#' }
 
-  myfunc = chebyshev_coef(f,lower = l, upper = u, verbose = T, tol = 1E-14)
-  plot(log10(abs(myfunc)), type = "s")
-  abline(v = chebyshev_bestN(f, lower = l, upper = u, tol = 1E-14))
+CInfParabolaSmoother <- function(xvec, yvec) {
+  # Check the lengths to ensure that they are the same
+  stopifnot(length(xvec) == length(yvec))
+  # Check to see that we have enough points to make this work
+  stopifnot(length(xvec) >= 4)
+  
+  # Sort the yvec and the xvec to ensure stable ordering
+  myorder = order(xvec)
+  xvec <- xvec[myorder]
+  yvec <- yvec[myorder]
+  N = length(xvec)
+  
+  # Create the vectorized function
+  result <- function(x) {
+    sapply(x, function(loopx) {
+      # Check the boundary conditions
+      stopifnot(loopx >= min(xvec))
+      stopifnot(loopx <= max(xvec))
+      
+      # If the value is equal to any of the inputs, then just return the input
+      if (any(loopx == xvec)) {
+        return(yvec[loopx == xvec])
+      }
+      
+      # Find the breaking point
+      breakP <- max(which(xvec <= loopx))
+      cat(sprintf(">> Loopx at:      %.4f\n", loopx))
+      cat(sprintf("   Breakpoint at: %i -> [%0.0f,%0.0f]\n", breakP, xvec[breakP], xvec[breakP + 1]))
+      if (breakP == 1) {
+        # We only are using the first three points to approximate it
+        return(parabolaInterpolate(loopx, xvec[1:3], yvec[1:3]))
+      } else if (breakP == N - 1) {
+        # We are only using the last three points to approximate it
+        indicies = seq(N-2,N)
+        return(parabolaInterpolate(loopx, xvec[indicies], yvec[indicies]))
+      }
+      
+      # Else, we are using two approximations to weight these
+      # browser()
+      weight = (loopx - xvec[breakP]) / (xvec[breakP + 1] - xvec[breakP])
+      if (is.na(weight)) {
+        browser()
+      }
+      w.first = meroSFunc(weight)
+      w.second = 1 - w.first
+      # Get the left and the right parabola approximations centered on the
+      # points breakP and breakP + 1
+      leftIndicies = seq(breakP-1, breakP + 1)
+      rightIndicies = seq(breakP, breakP + 2)
+      leftApprox = parabolaInterpolate(loopx, xvec[leftIndicies], yvec[leftIndicies])
+      rightApprox = parabolaInterpolate(loopx, xvec[rightIndicies], yvec[rightIndicies])
+      
+      # Return the approximation
+      return(w.second * leftApprox + w.first * rightApprox)
+    })
+  }
+}
+
 
 
 
